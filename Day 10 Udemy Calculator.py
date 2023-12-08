4ae40d7ad3520aa5801d0d89b24704ffd2ddf1f7
Bayden Willms <90474084+baydenwillms@users.noreply.github.com>
2023-12-08 16:09:15 -0500

    Add files via upload

---
+++ Day 10 Udemy Calculator.py
@@ -0,0 +1,62 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+
+    for symbol in operations: 
+        print(symbol)
+
+    should_continue = True
+
+    while should_continue:
+
+        operation_symbol = input("Choose an operation: ")
+        num2 = float(input("What's the next number?: "))
+
+        calculation = operations[operation_symbol]
+        answer = calculation(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input("Type 'y' to continue calculating with {answer}, or 'n' to start a new calculation.: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator() #this is the first use of recursion in this course
+
+calculator()
